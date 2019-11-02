696031aa19ce0dd7d071d58bfdef33e4f667a653
dariodenardi <dariodenardi@hotmail.it>
2019-11-02 10:05:22 +0100

    Bundles

--- TobaccoNicotineApplication/TobaccoNicotineApplication/App_Start/BundleConfig.cs
+++ TobaccoNicotineApplication/TobaccoNicotineApplication/App_Start/BundleConfig.cs
@@ -9,50 +9,77 @@ namespace TobaccoNicotineApplication
         public static void RegisterBundles(BundleCollection bundles)
         {
             // script
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new ScriptBundle("~/Scripts/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            bundles.Add(new ScriptBundle("~/Scripts/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new ScriptBundle("~/Scripts/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
             // Utilizzare la versione di sviluppo di Modernizr per eseguire attività di sviluppo e formazione. Successivamente, quando si è
             // pronti per passare alla produzione, usare lo strumento di compilazione disponibile all'indirizzo https://modernizr.com per selezionare solo i test necessari.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            bundles.Add(new ScriptBundle("~/Scripts/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/detect").Include(
+            bundles.Add(new ScriptBundle("~/Scripts/detect").Include(
                         "~/Scripts/detect.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/fastclick").Include(
+            bundles.Add(new ScriptBundle("~/Scripts/fastclick").Include(
                         "~/Scripts/fastclick.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryslim").Include(
+            bundles.Add(new ScriptBundle("~/Scripts/jqueryslim").Include(
                         "~/Scripts/jquery.slimscroll.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryblock").Include(
+            bundles.Add(new ScriptBundle("~/Scripts/jqueryblock").Include(
                         "~/Scripts/jquery.blockUI.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/waves").Include(
+            bundles.Add(new ScriptBundle("~/Scripts/waves").Include(
                         "~/Scripts/waves.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/wow").Include(
+            bundles.Add(new ScriptBundle("~/Scripts/wow").Include(
                         "~/Scripts/wow.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jquerynice").Include(
+            bundles.Add(new ScriptBundle("~/Scripts/jquerynice").Include(
                         "~/Scripts/jquery.nicescroll.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryscroll").Include(
+            bundles.Add(new ScriptBundle("~/Scripts/jqueryscroll").Include(
                         "~/Scripts/jquery.scrollTo.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+            bundles.Add(new ScriptBundle("~/Scripts/app").Include(
                         "~/Scripts/app.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/cookies").Include(
+            bundles.Add(new ScriptBundle("~/Scripts/cookies").Include(
                         "~/Scripts/jquery.cookie.js",
                         "~/Scripts/cookies_eu.js"));
+
+            bundles.Add(new ScriptBundle("~/Scripts/pagination").Include(
+                        "~/Scripts/pagination.js"));
+
+            bundles.Add(new ScriptBundle("~/Scripts/colorpicker").Include(
+                        "~/Scripts/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js"));
+
+            bundles.Add(new ScriptBundle("~/Scripts/maxlength").Include(
+                        "~/Scripts/plugins/bootstrap-maxlength/bootstrap-maxlength.min.js"));
+
+            bundles.Add(new ScriptBundle("~/Scripts/sweetalert").Include(
+                        "~/Scripts/plugins/bootstrap-sweetalert/sweet-alert.min.js"));
+
+            bundles.Add(new ScriptBundle("~/Scripts/filestyle").Include(
+                        "~/Scripts/plugins/bootstrap-filestyle/js/bootstrap-filestyle.min.js"));
+
+            bundles.Add(new ScriptBundle("~/Scripts/dropzone").Include(
+                        "~/Scripts/plugins/dropzone/dist/dropzone.js"));
+
+            bundles.Add(new ScriptBundle("~/Scripts/parsley").Include(
+                        "~/Scripts/plugins/parsleyjs/parsley.min.js"));
+
+            bundles.Add(new ScriptBundle("~/Scripts/jvectormap").Include(
+                        "~/Scripts/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js",
+                        "~/Scripts/plugins/jvectormap/jquery-jvectormap-world-mill-en.js",
+                        "~/Scripts/plugins/jvectormap/gdp-data.js",
+                        "~/Scripts/plugins/jvectormap/jvectormap.init.js"));
 
             // style
             bundles.Add(new StyleBundle("~/Content/css").Include(
@@ -64,6 +91,23 @@ namespace TobaccoNicotineApplication
 
             bundles.Add(new StyleBundle("~/Content/cookies").Include(
                       "~/Content/cookies_eu.css"));
+
+            bundles.Add(new StyleBundle("~/Content/colorpicker").Include(
+                      "~/Scripts/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/sweetalert").Include(
+                      "~/Scripts/plugins/bootstrap-sweetalert/sweet-alert.css"));
+
+            bundles.Add(new StyleBundle("~/Content/dropzone").Include(
+                      "~/Scripts/plugins/dropzone/dist/dropzone.css"));
+
+            bundles.Add(new StyleBundle("~/Content/jvectormap").Include(
+                      "~/Scripts/plugins/jvectormap/jquery-jvectormap-2.0.2.css"));
+
+            bundles.Add(new StyleBundle("~/Content/datatables").Include(
+                      "~/Scripts/plugins/datatables/jquery.dataTables.min.css",
+                      "~/Scripts/plugins/datatables/select.bootstrap.min.css",
+                      "~/Scripts/plugins/datatables/dataTables.bootstrap.min.css"));
 
             // you will need the line below so already minified files can be picked up by the bundler in debug mode.
             BundleTable.EnableOptimizations = true;
